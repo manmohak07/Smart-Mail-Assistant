8634f6a2dc4ebfcdb16e85aa0c599672d6e8931c
Mohak Agrawal <agrawalmohak988@gmail.com>
2025-04-01 22:45:05 +0530

    intial commit

---
+++ email-writer-react/src/api.js
@@ -0,0 +1,21 @@
+const backendUrl = "https://adept-figure-454319-h1.el.r.appspot.com";
+
+export const generateReply = async (emailText, tone = "") => {
+    try {
+        const response = await fetch(`${backendUrl}/api/email/generate`, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ 
+                emailContent: emailText, // Changed from "email" to "emailContent"
+                tone: tone 
+            }),
+        });
+        if (!response.ok) {
+            throw new Error("Failed to fetch reply");
+        }
+        return await response.text();
+    } catch (error) {
+        console.error("Error generating reply:", error);
+        throw error;
+    }
+};
